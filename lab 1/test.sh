71f4819d783c0d05243ac8790a5db81301857e83
Lin Jianghao <chiangel.ljh@gmail.com>
2020-03-16 10:53:10 +0800

    Lab 01: modify test.sh

--- lab 1/test.sh
+++ lab 1/test.sh
@@ -4,7 +4,7 @@ epoch=1
 
 while true
 do
-    echo "$epoch"
+    echo "Epoch: $epoch"
     for sim_time in 1000 1500 2000 3000 5000 10000 20000
     do
         for mean_msg_arrivalint in 0.01 0.1 0.2 0.3 0.5 0.8 1
@@ -17,12 +17,11 @@ do
                     do
                         for corrupt in 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
                         do
-                            echo "Epoch $epoch: $sim_time $mean_msg_arrivalint $mean_msg_size $outoforder $loss $corrupt"
                             ./rdt_sim $sim_time $mean_msg_arrivalint $mean_msg_size $outoforder $loss $corrupt 0 > rdt_sim.log
                             if [ $(grep -c "\<Congratulations\>" rdt_sim.log) -lt 1 ]; 
                             then 
+                                echo "Epoch $epoch: $sim_time $mean_msg_arrivalint $mean_msg_size $outoforder $loss $corrupt"
                                 echo "ERROR: something wrong !"
-                                exit
                             fi
                         done
                     done
