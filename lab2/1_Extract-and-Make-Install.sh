af0c7e5f4f35cdbd2375383f6080156035ffea6b
Lin Jianghao <chiangel.ljh@gmail.com>
2020-03-16 17:02:57 +0800

    modify shells

--- lab2/1_Extract-and-Make-Install.sh
+++ lab2/1_Extract-and-Make-Install.sh
@@ -1,3 +1,3 @@
-tar xf dpdk-16.11.11.tar.xz # 解压源码
-cd dpdk-stable-16.11.11 # 进入源码目录
+tar xf dpdk-16.11.8.tar.gz # 解压源码
+cd dpdk-stable-16.11.8 # 进入源码目录
 make install T=x86_64-native-linuxapp-gcc # 安装 DPDK
